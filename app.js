b5e3753c376001891dfd0001dd903d578e5a044b
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-05-08 15:19:55 +0530

    Todo

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
-//Selectors
+//SELECTORS
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-//Event Listeners
+//EVENTLISTENER
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
-//Functions
+//FUNCTIONS
 function addTodo(event) {
-  //Preventing form submitting
+  //PREVENTING FROM SUBMITTING
   event.preventDefault();
-  //Todo DIV
+
+  //TODO DIV
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
-  //Create LI
+  //CREATE NEW-TODO
   const newTodo = document.createElement("li");
-  newTodo.innerText = "Coffe added";
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
-  
+
+  //CHECK MARK-BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.classList.add("complete-button");
+  completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+  todoDiv.appendChild(completedButton);
+
+  //DELETE-BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+  trashButton.classList.add("trash-button");
+  todoDiv.appendChild(trashButton);
+
+  //APPEND DIV TO LIST
+  todoList.appendChild(todoDiv);
+  todoInput.value = "";
+}
+
+function deleteCheck(items) {
+  //GETTING ITEMS BY PARAMS
+  const todos = items.target;
+
+  //DELETE TODO
+  const todo = items.parentElement;
+  todo.remove()
 }
